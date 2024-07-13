5180aa6f5e1b2ca53711d0c3e70843f0b4f0be6b
JanLahmann <jan@lahmann-online.de>
2024-07-13 19:39:50 +0200

    fix

--- config/env-config.sh
+++ config/env-config.sh
@@ -2,7 +2,11 @@
 # shellcheck disable=SC2046
 if [ -e /home/$SUDO_USER/$REPO/config/env ] ; then
   export $(grep -v "^#" "/home/$SUDO_USER/$REPO/config/env" | xargs -d "\n")
+else
+  echo "/home/$SUDO_USER/$REPO/config/env not found"
 fi
 if [ -e /home/$USER/$REPO/config/env ] ; then
   export $(grep -v "^#" "/home/$USER/$REPO/config/env" | xargs -d "\n")
+else
+  echo "/home/$USER/$REPO/config/env not found"
 fi
