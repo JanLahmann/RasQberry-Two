406735a4c56c51276eb63e473e277cc1bc38e869
JanLahmann <jan@lahmann-online.de>
2024-07-13 18:51:58 +0200

    fix

--- config/env-config.sh
+++ config/env-config.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # shellcheck disable=SC2046
-export $(grep -v "^#" "/home/$SUDO_USER/$REPO/config/RQB2.env" | xargs -d "\n")
+export $(grep -v "^#" "/home/$SUDO_USER/$REPO/config/env" | xargs -d "\n")
