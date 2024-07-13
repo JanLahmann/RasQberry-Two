f7e7b5c3e169e70ec7e59fb143f24279cb5fe0c6
JanLahmann <jan@lahmann-online.de>
2024-07-13 17:28:29 +0200

    fix

--- config/env-config.sh
+++ config/env-config.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # shellcheck disable=SC2046
-source /home/$SUDO_USER/$REPO/RQB2-initial.env
+#echo "will source /home/$SUDO_USER/$REPO/RQB2-initial.env"
+#source /home/$SUDO_USER/$REPO/RQB2-initial.env
+export ENV="RQB2.env"
 export $(grep -v "^#" "/home/$SUDO_USER/$REPO/config/$ENV" | xargs -d "\n")
