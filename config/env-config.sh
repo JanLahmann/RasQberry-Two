39c0baff8765c036a12344fcf4d50a33f35bf454
JanLahmann <jan@lahmann-online.de>
2024-07-13 22:56:23 +0200

    fix

--- config/env-config.sh
+++ config/env-config.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # shellcheck disable=SC2046
-RQB2_CONFDIR=.local/config # Attention: variable is defined in multiple files
+
+# Attention: variable is defined in multiple files
+RQB2_CONFDIR=.local/config 
 if [ -e /home/$SUDO_USER/$RQB2_CONFDIR/env ] ; then
   export $(grep -v "^#" "/home/$SUDO_USER/$RQB2_CONFDIR/env" | xargs -d "\n")
 elif [ -e /home/$USER/$RQB2_CONFDIR/env ] ; then
