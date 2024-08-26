bbd6f1471b5d81bbe728306ac67cc8533c8fac5e
Rishi Mathur <rishimathur14@gmail.com>
2024-08-26 17:32:30 +0530

    Update RQB2_menu.sh

--- config/RQB2_menu.sh
+++ config/RQB2_menu.sh
@@ -5,14 +5,14 @@
 ### Initial
 # Added By Rishi as part of rasQberry-two project 
 ### Initial
-export RQB2_CONFDIR=.local/config
-[ ! -d /home/$SUDO_USER/.local/bin ] && sudo  mkdir -p /home/$SUDO_USER/.local/bin
-[ ! -d /home/$SUDO_USER/$RQB2_CONFDIR ] && sudo  mkdir -p /home/$SUDO_USER/$RQB2_CONFDIR
-sudo chmod -R 755 /home/$SUDO_USER/.local/bin
-sudo chmod -R 755 /home/$SUDO_USER/$RQB2_CONFDIR
-sudo cp /usr/bin/rq*.sh /home/$SUDO_USER/.local/bin
-sudo cp /usr/config/* /home/$SUDO_USER/$RQB2_CONFDIR
-#ln -sf ~/$RQB2_CONFDIR/env-config.sh ~/.local/bin
+# export RQB2_CONFDIR=.local/config
+# [ ! -d /home/$SUDO_USER/.local/bin ] && sudo  mkdir -p /home/$SUDO_USER/.local/bin
+# [ ! -d /home/$SUDO_USER/$RQB2_CONFDIR ] && sudo  mkdir -p /home/$SUDO_USER/$RQB2_CONFDIR
+# sudo chmod -R 755 /home/$SUDO_USER/.local/bin
+# sudo chmod -R 755 /home/$SUDO_USER/$RQB2_CONFDIR
+# sudo cp /usr/bin/rq*.sh /home/$SUDO_USER/.local/bin
+# sudo cp /usr/config/* /home/$SUDO_USER/$RQB2_CONFDIR
+# ln -sf ~/$RQB2_CONFDIR/env-config.sh ~/.local/bin
 
 SOURCE_FILE=/home/$SUDO_USER/"$RQB2_CONFDIR/env-config.sh"
 TARGET_LINK=/home/$SUDO_USER/.local/bin/env-config.sh
