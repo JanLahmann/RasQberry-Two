56cdcbdda8863f33ba30ab3eb4438d89b25494e3
Rishi Mathur <rishimathur14@gmail.com>
2024-08-26 16:24:16 +0530

    Update rq_install_Qiskit_latest.sh
    updated script use directly in the image building

--- bin/rq_install_Qiskit_latest.sh
+++ bin/rq_install_Qiskit_latest.sh
@@ -3,14 +3,19 @@
 # installation of Qiskit (latest version)
 #
 
-# Load environment variables
-. $HOME/.local/bin/env-config.sh
-
+#If no parameter passed
+if [ -z "$1" ]; then
+   # Load environment variables
+  . $HOME/.local/bin/env-config.sh
+  . $HOME/$REPO/venv/$STD_VENV/bin/activate
+else
+  # if images building parameter passed 
+  .  /home/"${FIRST_USER_NAME}"/$REPO/venv/$STD_VENV/bin/activate
+fi
 
 export STARTDATE=`date`
 echo; echo; echo "Install Qiskit (latest version)"; echo;
 
-. $HOME/$REPO/venv/$STD_VENV/bin/activate
 pip install 'qiskit[all]'
 
 
