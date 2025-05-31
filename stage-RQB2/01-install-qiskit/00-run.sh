77e1abfa8abfecfb3c9e5abc6d8a2b286aca0bb8
JanLahmann <jan@lahmann-online.de>
2025-05-31 10:29:22 +0200

    fix script-dir vs stage-dir

--- stage-RQB2/01-install-qiskit/00-run.sh
+++ stage-RQB2/01-install-qiskit/00-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 # Copy the stage config file directly
-cp "${SCRIPT_DIR}/../stage-config" "${ROOTFS_DIR}/tmp/stage-config"
+cp "${STAGE_DIR}/stage-config" "${ROOTFS_DIR}/tmp/stage-config"
